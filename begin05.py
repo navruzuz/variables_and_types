4b009d7bbaac41bf47e98f49868d43b113e40b9b
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-29 12:07:53 +0500

    bajarildi

--- begin05.py
+++ begin05.py
@@ -1,7 +1,8 @@
 # Assign 7 (int value) to 'a' variable;
-
+a=int(7)
 # Assign 3 (int value) to 'b' variable;
-
+b=int(3)
 # Assign 5 (int value) to 'c' variable;
-
+c=int(5)
 # Print the sum of three variables 'a', 'b', 'c';
+print(a+b+c)
